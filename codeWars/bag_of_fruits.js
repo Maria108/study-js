f136192660dd9b50fea6d80b5bf97fb703ded3c8
Maria108 <maria.abash@gmail.com>
2018-07-13 16:49:01 -0400

    codeWars

---
+++ codeWars/bag_of_fruits.js
@@ -0,0 +1,24 @@
+/* Our fruit guy has a bag of fruit (represented as an array of strings) where some fruits are rotten. He wants to replace all the rotten pieces of fruit with fresh ones. For example, given ["apple","rottenBanana","apple"] the replaced array should be ["apple","banana","apple"]. Your task is to implement a method that accepts an array of strings containing fruits should returns an array of strings where all the rotten fruits are replaced by good ones.
+
+Notes
+If the array is null/nil/None or empty you should return empty array ([]).
+The rotten fruit name will be in this camelcase (rottenFruit).
+The returned array should be in lowercase. */
+
+function removeRotten(bagOfFruits) {
+  if (!bagOfFruits) {
+    return [];
+  }
+  let output = [];
+  for (let i = 0; i < bagOfFruits.length; i++) {
+    if (bagOfFruits[i].slice(0, 6) === 'rotten') {
+      output.push(bagOfFruits[i].slice(6).toLowerCase());
+    } else {
+      output.push(bagOfFruits[i].toLowerCase());
+    }
+  }
+  return output;
+}
+
+console.log(removeRotten(['apple', 'banana', 'kiwi', 'melone', 'orange']));
+console.log(removeRotten([]));
