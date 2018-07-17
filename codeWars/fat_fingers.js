b9b12680b86dda1d0fdac5dc4913baf25b613557
Maria108 <maria.abash@gmail.com>
2018-07-17 16:12:05 -0400

    codeWars

--- codeWars/fat_fingers.js
+++ codeWars/fat_fingers.js
@@ -1,14 +1,22 @@
 var fatFingers = function(str) {
-    for (i = 0; i < str.length; i++) {
-        if (str[i] === "a") {
-            str = str.slice(0, i).concat(str.slice(i + 1).toUpperCase())
-        }
-        if (str[i] === "A") {
-            str = str.slice(0, i).concat(str.slice(i + 1).toLowerCase())
-        }
+  if (str === null) {
+    return null;
+  }
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === 'a') {
+      str = str.slice(0, i).concat(str.slice(i + 1).toUpperCase());
+      i = 0;
     }
-    return str
+    if (str[i] === 'A') {
+      str = str.slice(0, i).concat(str.slice(i + 1).toLowerCase());
+      i = 0;
+    }
+  }
+  if (str.length === 1 && str.toLowerCase() === 'a') {
+    return '';
+  }
+  return str;
 };
 
-console.log(fatFingers("The aAn quick Abrown an fox jumps over the lazy dogamsfa."))
-console.log(fatFingers("aaa"))
+// console.log(fatFingers('The a quick brown fox jumps over the lazy dog.'));
+console.log(fatFingers('aaAAaAaaAAAa'));
