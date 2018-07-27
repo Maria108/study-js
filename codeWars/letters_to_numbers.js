472f53a2800680748f0575635f572f9644be244a
Maria108 <maria.abash@gmail.com>
2018-07-27 15:25:24 -0400

    codeWars

--- codeWars/letters_to_numbers.js
+++ codeWars/letters_to_numbers.js
@@ -27,3 +27,22 @@ function parse(data) {
   }
   return output;
 }
+
+/* var map = {
+  'i' : function(x){return x + 1;},
+  'd' : function(x){return x - 1;},
+  's' : function(x){return x * x;}
+}
+
+function parse( data )
+{
+  var array = [], val = 0;
+  data.split('').forEach(function(x){
+    if(x === 'o'){
+      array.push(val); 
+    } else {
+      val = map[x](val);
+    }
+  });
+  return array;
+} */
