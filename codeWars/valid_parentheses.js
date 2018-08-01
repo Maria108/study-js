0e55edce0de6f6e8630d1f5910bfc418d5e54679
Maria108 <maria.abash@gmail.com>
2018-08-01 17:14:11 -0400

    codeWars

---
+++ codeWars/valid_parentheses.js
@@ -0,0 +1,32 @@
+/* Write a function called that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+
+Examples
+"()"              =>  true
+")(()))"          =>  false
+"("               =>  false
+"(())((()())())"  =>  true
+Constraints
+0 <= input.length <= 100
+
+You may assume that the input string will only contain opening and closing parenthesis and will not be an empty string. */
+
+function validParentheses(parens) {
+  let openPar = 0;
+  let closePar = 0;
+  if (parens.length === 1 || parens[0] === ')') {
+    return false;
+  }
+  for (let i = 0; i < parens.length; i++) {
+    parens[i] === '(' ? openPar++ : closePar++;
+    if (closePar > openPar) {
+      return false;
+    }
+  }
+  if (openPar === closePar) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(validParentheses('(())((()())())'));
