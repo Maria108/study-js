a2d0ae75a78ebf60f29c24e06ef586a35cd53cd7
Maria108 <maria.abash@gmail.com>
2018-08-08 12:23:56 -0400

    codeWars

---
+++ codeWars/math_methods.js
@@ -0,0 +1,45 @@
+/* Oh no, our Math object was "accidently" reset. Can you re-implement some of those functions? We can assure, that only non-negative numbers are passed as arguments. So you don't have to consider things like undefined, null, NaN, negative numbers, strings and so on.
+
+Here is a list of functions, we need:
+
+Math.round()
+Math.ceil()
+Math.floor() */
+
+console.log(000000001 < 0);
+
+round = function(number) {
+  let num = number.toString();
+  if (num.length > 1) {
+    let arr = num.split('.');
+    if (arr[1][0] >= 5) {
+      return parseInt(arr[0]) + 1;
+    } else {
+      return parseInt(arr[0]);
+    }
+  } else {
+    return number;
+  }
+};
+
+Math.ceil = function(number) {
+  let num = number.toString();
+  if (num.length > 1) {
+    let arr = num.split('.');
+    if (arr[1] > 0) {
+      return parseInt(arr[0]) + 1;
+    } else {
+      return parseInt(arr[0]);
+    }
+  } else {
+    return number;
+  }
+};
+
+Math.floor = function(number) {
+  let num = number.toString();
+  let arr = num.split('.');
+  return parseInt(arr[0]);
+};
+
+console.log(round(4));
