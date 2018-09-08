e62b86ea11f29adc8cf843996b377794eaff43c6
Maria108 <maria.abash@gmail.com>
2018-09-08 13:34:24 -0400

    codeWars

--- codeWars/multiplies_3_and_5.js
+++ codeWars/multiplies_3_and_5.js
@@ -4,6 +4,14 @@ Finish the solution so that it returns the sum of all the multiples of 3 or 5 be
 
 Note: If the number is a multiple of both 3 and 5, only count it once. */
 
-function solution(number){
-  
+function solution(number) {
+  let sum = 0;
+  for (let i = 1; i < number; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      sum += i;
+    } else if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
 }
